bfc93a4377fe000202b9a12f691a18dee2b28b5a
evs <evoraleite@gmail.com>
2019-12-17 17:30:01 -0300

    price ok

--- src/components/ItemAdded.js
+++ src/components/ItemAdded.js
@@ -5,7 +5,7 @@ import { StyleSheet, css } from 'aphrodite'
 export default function ItemAdded({children, ...props}) {
 
 	return (
-			<li {...props}>{children} <a className={css(style.trash)} onClick={() => props.onClick}><span  id={props.id} role="img" aria-label="botão de retirar item da lista">&#128465;</span></a></li>
+			<li {...props}>{children} <a className={css(style.trash)} onClick={() => props.onClick}><span id={props.id} role="img" aria-label="botão de retirar item da lista">&#128465;</span></a></li>
 	)
 }
 
--- src/pages/menuPage.js
+++ src/pages/menuPage.js
@@ -13,6 +13,7 @@ export default function MenuPage(props) {
 	const [menu, setMenu] = useState([])
 	const [tipoDeMenu, setTipoDeMenu] = useState('cafe')
 	const [order, setOrder] = useState([])
+	const [price, setPrice] = useState(0)
 
 	useEffect(() => {
 		db.collection('menu').get().then(snap => {
@@ -25,15 +26,15 @@ export default function MenuPage(props) {
 		setTipoDeMenu(e.target.id)
 	}
 
-	const addToList = (id) => {	
-		setOrder([...order, id])
+	const addToList = (item) => {	
+		setOrder([...order, `${item.target.title} R$ ${item.target.id},00`])
+		setPrice(Number(price) + Number(item.target.id))
 	}
 
 	const trash = (e) => {
-		setOrder(order.filter(i => i !== e.target.id))		
+		setOrder(order.filter(i => i !== e.target.id))	
+		setPrice(Number(price) - parseInt((e.target.id).slice(-5)))
 	}
-
-
 
 	return (
 		<div>
@@ -43,9 +44,10 @@ export default function MenuPage(props) {
 				<Input type='number' id='costumer-number' placeholder='Número da mesa' required />
 				<ul id='order-list'>
 					{
-						order.map(i => <ItemAdded key={Math.random()} id={i} children={i} onClick={trash} />)
+						order.map(i => <ItemAdded key={Math.random()} id={i} children={i} onClick={(e) => trash(e)} />)
 					}
 				</ul>
+				<p>Total: {price}</p>
 			</form>			
 			<section className={css(style.buttonMenu)}>
 				<Button children='Café da Manhã' id='cafe' onClick={showMenu} />
@@ -55,7 +57,7 @@ export default function MenuPage(props) {
 				{['cafe', 'lanche'].filter(m => m === tipoDeMenu).map(categoria => 
 					<Menu key={Math.random()} children={
 						menu.filter(i => i.categoria === categoria).map(i => 
-							<MenuItem onClick={() => addToList(i.nome)}
+							<MenuItem onClick={(e) => addToList(e)} id={i.valor}
 							title={i.nome} key={i.nome} value={i.valor} />
 							)
 					}/>
